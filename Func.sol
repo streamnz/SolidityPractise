138e4143454c06678875427e44bce9e8bf80d6c7
streamnz <hao.streamnz@gmail.com>
2024-10-05 12:04:17 +0530

    add function practise

---
+++ Func.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract FuncIntro{
+    function add(uint a,uint b) external pure returns (uint256) {return a + b;}
+
+    function sub(uint a,uint b) external pure returns(uint256){return a - b;}
+
+}
